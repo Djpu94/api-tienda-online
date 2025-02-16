65aaf6aef7cab913ff01422eb8e98e071b762c3e
Daniel Perez <danieljpu2013@gmail.com>
2025-02-16 16:46:19 -0300

    servicio de Autenticacion

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { User, UserDocument } from './schemas/user.schema';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(email: string, password: string): Promise<any> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new this.userModel({ email, password: hashedPassword });
+    await user.save();
+    return { message: 'Usuario registrado correctamente' };
+  }
+
+  async login(email: string, password: string): Promise<any> {
+    const user = await this.userModel.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Credenciales incorrectas');
+    }
+
+    const payload = { email: user.email, role: user.role };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
